2844ea2c889b7699b024d896b8caf3bd66443f1e
christoffer-ridderland <christoffer.ridderland@gmail.com>
2025-05-02 19:34:24 +0200

    nightmate commit WIP hypr, wofi, waybar, wallust

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,5 @@ if status is-interactive
     neofetch --ascii ~/.config/incredible.txt
     # Commands to run in interactive sessions can go here
 end
+
+alias meow 'timeout 3s chafa -f symbols --symbols ascii -c 240 --scale max --align center ~/Downloads/hahacat.gif'
