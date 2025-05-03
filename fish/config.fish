4bd7f1b2588b976e57ebb387a3d1fcb26632e08f
christoffer-ridderland <christoffer.ridderland@gmail.com>
2025-05-03 04:20:39 +0200

    switched to fastfetch

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,11 @@
-if status is-interactive
-    neofetch --ascii ~/.config/incredible.txt
-    # Commands to run in interactive sessions can go here
+function fish_greeting
+    echo Rise and shine, (set_color yellow)comrade(set_color normal). It\'s time to get to work.
 end
 
+if status is-interactive
+    set -l img (find ~/Pictures/disco -type f | shuf -n 1);
+    fastfetch -c ~/.config/fastfetch/config.jsonc --logo-type chafa --logo-padding-left 2 --logo-height 25 -l $img
+end
+
+set -g fish_greeting
 alias meow 'timeout 3s chafa -f symbols --symbols ascii -c 240 --scale max --align center ~/Downloads/hahacat.gif'
